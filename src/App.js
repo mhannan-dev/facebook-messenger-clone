cbda22e59ba1a62a34837ad84a991c46138e0f34
MHannan <mdhannan.info@gmail.com>
2022-02-05 01:23:28 +0600

    working on show name and welcome

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { FormControl} from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
-import  Message  from './parts/Message'
+import { useState, useEffect } from 'react';
+import Message from './parts/Message'
 import './App.css';
+
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(['hi', 'hello', 'welcome']);
+  const [messages, setMessages] = useState([
+    { username: 'Sunny' }, { text: 'whatsapp' },
+    { username: 'Ahmed' }, { text: 'hello bro' }
+  ]);
+
+  const [username, setUsername] = useState('');
   console.log(input);
   console.log(messages);
+
+  useEffect(() => {
+    setUsername(prompt('Pleae enter your name'));
+  }, []);
   //Send message
   const sendMessage = (event) => {
+    event.preventDefault();
     setMessages([...messages, input]);
     setInput('');
   }
@@ -20,17 +31,18 @@ function App() {
   return (
     <div className="App">
       <h1>React, Firebase Facebook Messenger</h1>
+      <h3>Welcome {username}</h3>
       <form>
         <FormControl>
           <InputLabel>Enter a message below</InputLabel>
           <Input placeholder='Enter message here' onChange={event => setInput(event.target.value)} />
-          <Button  disabled={!input} type="submit" variant="contained" onClick={sendMessage}>Send Message</Button>
+          <Button disabled={!input} type="submit" variant="contained" onClick={sendMessage}>Send Message</Button>
         </FormControl>
       </form>
       {/* Showing Message */}
       {
         messages.map(message => (
-          <Message key={message.toString()} text={message}/>
+          <Message key={message.toString()} username={message.username} text={message.text} />
         ))
       }
 
