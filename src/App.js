0f099819b75d842d30f9170ca6aba72b424e2c95
MHannan <mdhannan.info@gmail.com>
2022-02-04 14:05:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { Button, FormControl, InputLabel} from '@material-ui/core';
+import { useState } from 'react';
+import  Message  from './parts/Message'
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hi', 'hello', 'welcome']);
+  console.log(input);
+  console.log(messages);
+  //Send message
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React, Firebase Facebook Messenger</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message</InputLabel>
+          <input placeholder='Enter text' onChange={event => setInput(event.target.value)} />
+          <Button type="submit" variant="contained" onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
+      {/* Showing Message */}
+      {
+        messages.map(message => (
+          <Message key={message.toString()} text={message}/>
+        ))
+      }
+
     </div>
   );
 }
-
 export default App;
---
+++ src/parts/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Message() {
+  return (
+    <div>
+      <p>Message</p>
+    </div>
+  );
+}
+export default Message
